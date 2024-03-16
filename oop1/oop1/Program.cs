e61b92c664c940e9b2fc54a62d448b3be94c176c
FakirHerif <basdemir35@hotmail.com.tr>
2024-03-16 07:27:34 +0300

    value types in stack

---
+++ oop1/oop1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Value types in stack
+
+int sayi1 = 10;     // stack'de değer 10
+int sayi2 = 20;     // stack'de değer 20
+sayi1 = sayi2;      // sayi1'in değeri 20 oldu
+sayi2 = 100;        // sayi2'nin değeri 100 oldu
+
+Console.WriteLine(sayi1);   // 20
+Console.ReadLine();
