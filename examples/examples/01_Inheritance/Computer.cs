36e6f725cac0257595cc74b856a8ef662319c1e8
FakirHerif <basdemir35@hotmail.com.tr>
2024-03-18 23:45:39 +0300

    added first part of inheritance example

---
+++ examples/examples/01_Inheritance/Computer.cs
@@ -0,0 +1,33 @@
+﻿namespace examples._01_Inheritance
+{
+    public class Computer   // Üst Sınıf
+    {
+        public int Id { get; set; }   // Property
+        public string CpuModel { get; set; }
+        public string MotherboardModel { get; set; }
+        public string RamModel { get; set; }
+        public string SsdModel { get; set; }
+        public string GpuModel { get; set; }
+
+        public Computer()   // Constructor
+        {
+            Console.WriteLine("Computer nesnesi başlatıldı");
+        }
+    }
+
+    public class Desktop : Computer
+    {
+        public string CaseModel { get; set; }
+        public string PsuModel { get; set; }
+        public bool LiquidCooling { get; set; }
+
+    }
+
+    public class Laptop : Computer
+    {
+        public string BatteryCapacity { get; set; }
+        public string ScreenSize { get; set; }
+        public string Color { get; set; }
+        public bool TouchScren { get; set; }
+    }
+}
---
+++ examples/examples/Program.cs
@@ -0,0 +1,14 @@
+﻿using examples._01_Inheritance;
+
+namespace examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer = new Computer();
+            
+            Console.ReadLine();
+        }
+    }
+}
