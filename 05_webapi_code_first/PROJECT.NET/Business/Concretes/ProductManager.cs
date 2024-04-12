12c9980a6a8bcc0f303c4de5262255cf107e207d
FakirHerif <basdemir35@hotmail.com.tr>
2024-04-13 00:58:17 +0300

    implemented Code-First with migrations

---
+++ 05_webapi_code_first/PROJECT.NET/Business/Concretes/ProductManager.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstracts;
+using DataAccess.Abstracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class ProductManager : IProductService   // Soyut sınıf olan IProductService(interface)'in karşılığı olan somut sınıf ProductManager oluşturulur ve bu sınıf IProductService'den kalıtım alınır. Implemente ettikten sonra metodlar yazılır
+    {
+
+        IProductRepository _productRepository;      // burada isimlendirme kuralında class içindeki global değişkenler c#'da alt tire ile yazılır
+
+        // DEPENDENCY INJECTION YAPARKEN HER ZAMAN AKLIMIZA ŞU SORU GELMELİ: BU SERVİS, SERVİSLER ARASINA EKLENDİ Mİ?
+
+        public ProductManager(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public void Add(Product product)
+        {
+            if(product.UnitPrice < 0)
+            {
+                throw new Exception("Ürün fiyatı 0'dan küçük olamaz");
+            }
+
+            _productRepository.Add(product);
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            // Cacheleme yapılıyorsa cahce'den gelsin gibi bir kural yazılabilir.
+            return _productRepository.GetAll();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 05_webapi_code_first/PROJECT.NET/DataAccess/Abstracts/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstracts
+{
+    public interface IProductRepository
+    {
+        Product GetById(int id);        
+        List<Product> GetAll();         
+        void Add(Product product);      
+        void Update(Product product);   
+        void Delete(Product product);
+    }
+}
