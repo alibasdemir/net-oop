289e1c3b16e2032fe0f29b743ce70e8af8ef91fd
FakirHerif <basdemir35@hotmail.com.tr>
2024-04-18 03:20:31 +0300

    implemented global exception handling and extension method

---
+++ 10_webapi_global_exception_handling_extension_method/PROJECT.NET/Business/Abstracts/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts
+{
+    public interface IProductService    // Servislerin bulunduğu soyut sınıfı(interface) oluşturduk ve buna karşılık gelen somut sınıf olan manager'ı oluşturmalıyız
+    {
+        Product GetById(int id);        // TEK BAŞINA BİR PRODUCT DÖNEN GETBYID METODU
+        Task<List<Product>> GetAll();         // PRODUCT SINIFINDAN OLUŞAN BİR LİSTE DÖNDÜRÜR. GETALL METODU ÇAĞRILDIĞINDA PRODUCT LİSTESİ DÖNDÜRÜLÜR
+        Task Add(Product product);      // ADD METODU ---> metodu async yazdığımız için Task yaptık
+        void Update(Product product);   // UPDATE METODU
+        void Delete(int id);            // DELETE METODU
+    }
+}
---
+++ 10_webapi_global_exception_handling_extension_method/PROJECT.NET/Business/Concretes/ProductManager.cs
@@ -0,0 +1,66 @@
+﻿using Business.Abstracts;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using DataAccess.Abstracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class ProductManager : IProductService   // Soyut sınıf olan IProductService(interface)'in karşılığı olan somut sınıf ProductManager oluşturulur ve bu sınıf IProductService'den kalıtım alınır. Implemente ettikten sonra metodlar yazılır
+    {
+
+        IProductRepository _productRepository;      // burada isimlendirme kuralında class içindeki global değişkenler c#'da alt tire ile yazılır
+
+        // DEPENDENCY INJECTION YAPARKEN HER ZAMAN AKLIMIZA ŞU SORU GELMELİ: BU SERVİS, SERVİSLER ARASINA EKLENDİ Mİ?
+
+        public ProductManager(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task Add(Product product)  // async olduğu için Task yaptık
+        {
+            // ÜRÜN FİYATI 0'DAN KÜÇÜK OLAMAZ
+            if(product.UnitPrice < 0)
+            {
+                throw new BusinessException("Ürün fiyatı 0'dan küçük olamaz");
+            }
+
+            // AYNI İSİMDE 2.ÜRÜN EKLENEMEZ
+            Product? productWithSameName = await _productRepository.GetAsync(p=> p.Name == product.Name);
+            if(productWithSameName is not null)
+            {
+                throw new System.Exception("Aynı isimde 2. ürün eklenemez");
+            }
+
+            await _productRepository.AddAsync(product);
+        }
+
+        public void Delete(int id)
+        {
+            Product? productToDelete = _productRepository.Get(p=>p.Id == id);
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Product>> GetAll()
+        {
+            // Cacheleme yapılıyorsa cahce'den gelsin gibi bir kural yazılabilir.
+            return await _productRepository.GetListAsync();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
