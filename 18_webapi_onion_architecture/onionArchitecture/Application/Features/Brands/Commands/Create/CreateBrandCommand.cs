492c3f8114dfe280595b0f6aaf90cf339e46f485
FakirHerif <basdemir35@hotmail.com.tr>
2024-05-06 00:40:12 +0300

    dto, mediator, cqrs

---
+++ 18_webapi_onion_architecture/onionArchitecture/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,41 @@
+﻿using Application.Repositories;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create
+{
+    public class CreateBrandCommand : IRequest<CreateBrandResponse>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreateBrandResponse>
+        {
+            private readonly IBrandRepository _brandRepository;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository)
+            {
+                _brandRepository = brandRepository;
+            }
+
+            public async Task<CreateBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                Brand brand = new()
+                {
+                    Name = request.Name,
+                };  // manuel mapping
+                await _brandRepository.AddAsync(brand);
+                return new CreateBrandResponse()
+                {
+                    Id = brand.Id,
+                    Name = brand.Name
+                };
+
+            }
+        }
+    }
+}
---
+++ 18_webapi_onion_architecture/onionArchitecture/WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.Brands.Commands.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BrandsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody]CreateBrandCommand command)
+        {
+            CreateBrandResponse response = await _mediator.Send(command);
+            return Ok(response);
+        }
+    }
+}
